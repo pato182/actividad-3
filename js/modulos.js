f073ee3bb7762dc14a61591458c6bddaaf92b171
Muras Patricio <muras.patricio@tecnica7.edu.ar>
2025-04-27 22:56:07 -0300

    Primer commit

---
+++ js/modulos.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let source = (document.title == "Indice" ? '"img/pokemon-2.png"' : '"../img/pokemon-2.png"')
+    let link = (document.title == "Indice" ? '' : '../')
+
+    const header = `<img src=${source} alt="Logo"><nav><ul><li><a href="${link}index.html">Equipo</a></li><li><a href="${link}html/a.html">Bolsa</a></li><li><a href="${link}html/b.html">Perfil</a></li></ul></nav>`;
+    const footer = '<p>Jesus Baute y Patricio Muras</p><p>© 2025 Liga Pokémon</p>';
+    document.querySelector('header').innerHTML = header;
+    document.querySelector('footer').innerHTML = footer;
+});
