332b2685d4608ca832e2bfc047355c79ac422f2d
jesus <jesusdanielba29@gmail.com>
2025-05-05 19:16:07 -0300

    cambio de ruta html en js

--- js/modulos.js
+++ js/modulos.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const header = `<img src="./img/pokemon-2.png" alt="Logo"><nav><i class="fa-solid fa-bars"></i><ul><li><a href="./index.html">Equipo</a></li><li><a href="./a.html">Bolsa</a></li><li><a href="./b.html">Perfil</a></li></ul></nav>`;
+    const header = `<img src="./img/pokemon-2.png" alt="Logo"><nav><i class="fa-solid fa-bars"></i><ul><li><a href="./index.html">Equipo</a></li><li><a href="./bolsa.html">Bolsa</a></li><li><a href="./perfil.html">Perfil</a></li></ul></nav>`;
     const footer = '<p>Jesus Baute y Patricio Muras</p><p>© 2025 Liga Pokémon</p>';
     document.querySelector('header').innerHTML = header;
     document.querySelector('footer').innerHTML = footer;
